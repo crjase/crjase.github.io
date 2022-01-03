dc523dac2b10022281dfe4820a924acb6aa5f611
crjase <crjase@gmail.com>
2022-01-04 02:58:00 +1100

    Added funny message when rick rolled up to 3

--- index.js
+++ index.js
@@ -10,16 +10,26 @@ function show_img(src, width, height, alt) {
 };
 
 // Create rickroll button
+let rolled = 1
+
 let rickroll_button = document.createElement("button");
 rickroll_button.id = "rickroll_button";
 rickroll_button.name = "Rick Astley";
 rickroll_button.innerHTML = "Rick Astley";
 rickroll_button.type = "submit";
 rickroll_button.addEventListener("click", function () {
-    show_img(
-        'https://github.com/crjase/RenaiCirculation-Theme/blob/main/RenaiCirculation.png?raw=true',
-        400, 250,
-        'alt text'
-    )
+    // if rolled up to three show message
+    if (rolled < 4) {
+        console.log('Successfully Rick Rolled', rolled, 'Times')
+        rolled += 1
+
+        show_img(
+            'https://c.tenor.com/ULJn9_2K17UAAAAi/wazatoz-rick-roll.gif',
+            400, 250,
+            'alt text'
+        )
+    } else {
+        alert('You have rick rolled yourself too many times! PLEASE, stop, for the greater good of your mental state')
+    }
 });
 document.getElementById("center").appendChild(rickroll_button)
