83182c2d28e6cbc5d5d663c75bdf98ec5d52c31a
crjase <crjase@gmail.com>
2022-01-04 04:41:33 +1100

    some changes

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ function show_img(src, width, height, alt) {
     document.getElementById("center").appendChild(img);
 };
 
+function officially_rolled() {
+    let h1 = document.createElement('h1')
+    let img = document.createElement('img')
+    h1.textContent = 'LOOK WHAT YOU\'VE DONE'
+    img.src="Storage/rick.gif"
+    img.width = 100
+    img.height = 200
+    img.alt = 'trolled'
+    document.getElementById("center").appendChild(h1);
+    document.getElementById("center").appendChild(img);
+}
+
 // Create rickroll button
 let rolled = 1
 
@@ -26,12 +38,67 @@ rickroll_button.addEventListener("click", function () {
         rolled += 1
 
         show_img(
-            'https://c.tenor.com/ULJn9_2K17UAAAAi/wazatoz-rick-roll.gif',
+            rel = "Storage/rick.gif",
             400, 250,
             'alt text'
         )
     } else {
         alert('You have rick rolled yourself too many times! PLEASE, stop, for the greater good of your mental state')
+    
+        // Remove 'rick astley' button
+        document.getElementById('rickroll_button').remove();
+
+        // Replace 'rick astley' button with 'give me more rolls' button
+        let more_rolls = document.createElement('button')
+        more_rolls.id = 'more_rolls'
+        more_rolls.name = 'more_rolls'
+        more_rolls.innerHTML = 'GIVE ME MORE RICK ROLES'
+        more_rolls.addEventListener('click', function () {
+            document.querySelectorAll("body > * > *").forEach((e) => { e.remove() });
+            // re-create <center>
+            document.getElementsByTagName("main").appendChild(YourCenterElementAgainHere);
+            // one, last roll...
+            officially_rolled()
+        });
+        document.getElementById("center").appendChild(more_rolls)
     }
 });
 document.getElementById("center").appendChild(rickroll_button)
+
+/* Thank you
+
+let rickroll_button = document.createElement("button");
+rickroll_button.id = "rickroll_button";
+rickroll_button.name = "Rick Astley";
+rickroll_button.innerHTML = "Rick Astley";
+rickroll_button.type = "submit";
+rickroll_button.addEventListener("click", onRickrollClicked);
+
+function onRickrollClicked() {
+    if (rolled < 4) {
+        console.log('Successfully Rick Rolled', rolled, 'Times')
+        rolled += 1
+
+        show_img(
+            rel = "Storage/rick.gif",
+            400, 250,
+            'alt text'
+        )
+    } else {
+        alert('You have rick rolled yourself too many times! PLEASE, stop, for the greater good of your mental state')
+    
+        let more_rolls = document.getElementById('rickroll_button');
+        more_rolls.removeEventListener(onRickrollClicked);
+
+        // Replace 'rick astley' button with 'give me more rolls' button
+        more_rolls.id = 'more_rolls'
+        more_rolls.name = 'more_rolls'
+        more_rolls.innerHTML = 'GIVE ME MORE RICK ROLES'
+        more_rolls.addEventListener('click', function () {
+            alert('this button is working :D')
+        });
+        
+        document.getElementById("center").appendChild(more_rolls)
+    }
+}
+*/
