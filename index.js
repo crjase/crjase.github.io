ff634614e5fa74831a74dfab33120496deeb4478
MuhammadDanialFarooq <danialfarooqcoolboy2@hotmail.com>
2022-01-03 21:04:43 +0500

    this is making me crazy just add the link your self

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Reusable image display
 function show_img(src, width, height, alt) {
+    let br1 = document.createElement('br')
+    let br2 = document.createElement('br')
     let img = document.createElement('img')
     img.src = src
     img.width = width
     img.height = height
     img.alt = alt
-
+    document.getElementById("center").appendChild(br1);
+    document.getElementById("center").appendChild(br2);
     document.getElementById("center").appendChild(img);
 };
 
@@ -18,7 +21,6 @@ rickroll_button.name = "Rick Astley";
 rickroll_button.innerHTML = "Rick Astley";
 rickroll_button.type = "submit";
 rickroll_button.addEventListener("click", function () {
-    // if rolled up to three show message
     if (rolled < 4) {
         console.log('Successfully Rick Rolled', rolled, 'Times')
         rolled += 1
