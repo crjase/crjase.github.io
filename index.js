34fe27d10ed7aba21e612ab617135d61164b1af4
MuhammadDanialFarooq <danialfarooqcoolboy2@hotmail.com>
2022-01-04 16:40:22 +0500

    Design Unified 1

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ function show_img(src, width, height, alt) {
     img.width = width
     img.height = height
     img.alt = alt
+    document.getElementById("center").appendChild(img);
     document.getElementById("center").appendChild(br1);
     document.getElementById("center").appendChild(br2);
-    document.getElementById("center").appendChild(img);
 };
 
 function officially_rolled() {
     let h1 = document.createElement('h1')
     let img = document.createElement('img')
     h1.textContent = 'LOOK WHAT YOU\'VE DONE'
+    h1.style = "color:whitesmoke;"
     img.src="Storage/rick.gif"
     img.width = 100
     img.height = 200
@@ -26,8 +27,11 @@ function officially_rolled() {
 
 // Create rickroll button
 let rolled = 1
-
-let rickroll_button = document.createElement("button");
+let bre1 = document.createElement('br')
+let bre2 = document.createElement('br')
+let rickroll_button = document.createElement("a");
+rickroll_button.classList = "hoverbtn";
+rickroll_button.href = "#";
 rickroll_button.id = "rickroll_button";
 rickroll_button.name = "Rick Astley";
 rickroll_button.innerHTML = "Rick Astley";
@@ -49,21 +53,27 @@ rickroll_button.addEventListener("click", function () {
         document.getElementById('rickroll_button').remove();
 
         // Replace 'rick astley' button with 'give me more rolls' button
-        let more_rolls = document.createElement('button')
+        let more_rolls = document.createElement('a')
+        more_rolls.classList = "hoverbtn"
+        more_rolls.href = "#"
         more_rolls.id = 'more_rolls'
         more_rolls.name = 'more_rolls'
         more_rolls.innerHTML = 'GIVE ME MORE RICK ROLES'
         more_rolls.addEventListener('click', function () {
             document.querySelectorAll("body > * > *").forEach((e) => { e.remove() });
-            // re-create <center>
-            document.getElementsByTagName("main").appendChild(YourCenterElementAgainHere);
-            // one, last roll...
-            officially_rolled()
-        });
+               // re-create <center>
+               let center = document.createElement('center');
+               center.setAttribute('id', 'center');
+               document.getElementsByTagName('main')[0].appendChild(center) // needs fixing later
+               // one, last roll...
+               officially_rolled()
+           });
         document.getElementById("center").appendChild(more_rolls)
     }
 });
 document.getElementById("center").appendChild(rickroll_button)
+document.getElementById("center").appendChild(bre1);
+document.getElementById("center").appendChild(bre2);
 
 /* Thank you
 
